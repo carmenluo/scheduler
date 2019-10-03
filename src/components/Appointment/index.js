ef89e7efb9991a73e100b9c0158aaabf02d81687
carmenluo <Sdluojm@gmail.com>
2019-10-02 21:25:48 -0400

    update custome hook

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,7 +3,10 @@ import "./styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import useVisualMode from "hooks/useVisualMode";
 import { action } from "@storybook/addon-actions";
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
 export default function Appointment(props) {
   return props.interview
     ? <><Header time={props.time} /><Show student={props.interview.student}
